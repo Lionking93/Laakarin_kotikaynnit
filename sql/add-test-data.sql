c83fee0cc6aeb7d19d6f933287997c29e7cd01be
Leo Kallonen <leo@leo-laptop.(none)>
2014-11-18 10:22:09 +0200

    tietokantapäivitys

---
+++ sql/add-test-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO asiakas(nimi, tunnus, salasana, syntymaaika, henkilotunnus, osoite) VALUES ('Erkki Esimerkki', 'admin', 'admin', '1976-02-11', '110276-357K', 'Kaivokatu 5, 00100 Helsinki');
+
+INSERT INTO laakari(nimi, tunnus, salasana) VALUES ('Pentti Virtanen', 'laakari', 'laakari');
+
+INSERT INTO potilasraportti(lisaysajankohta, raportti) VALUES ('2014-14-11 09:15', 'Potilaalla on flunssaa ja kurkku kipeä.');
+
+INSERT INTO hoito-ohje(lisaysajankohta, ohje) VALUES ('2014-14-11 10:04', 'Yskänlääkettä, pari buranaa ja kunnon yöunet.');
+
+INSERT INTO varattava_aika(viikonpaiva, aika) VALUES('2014-20-11', '09:00-09:45');
---
+++ sql/drop-tables.sql
@@ -0,0 +1,5 @@
+DROP TABLE hoito-ohje;
+DROP TABLE potilasraportti;
+DROP TABLE varattava_aika;
+DROP TABLE laakari;
+DROP TABLE asiakas;
